ec702fcfdd905e37ac6afdcbbcef3ed6d422c240
josueRdgz <josuerdgz.ramirez@gmail.com>
2025-03-31 13:37:40 -0600

    Time limit exceeded on test 8

--- H_CoffeeBreak.py
+++ H_CoffeeBreak.py
@@ -1,53 +1,49 @@
+from collections import deque
+
+
 def CoffeeBreak(n):
-    maximum = []
-    
+    maximum = [0] * len(n)
+
     for i in range(len(n)):
         duplicated = n.copy()
-        
-        first_move=False
-        j=0
-        while j <len(n):
-            if j==i:
-                j+=1
-                continue
-            if duplicated[j]<2:
-                j+=1
-                continue
-            if j-1>=0:
-                duplicated[j-1]+=int(duplicated[j]/2)
-            if j+1<len(n):
-                duplicated[j+1]+=int(duplicated[j]/2)
-            if not duplicated[j]%2:
-                duplicated[j]=0
-            else:
-                duplicated[j]=1
-            
-            first_move=True
-            j+=1
-            for k in range(0,len(n)):
-                if duplicated[k]>=2 and k!=i:
-                    j=k
-                    break
-            if not first_move:
-                break
-                
-        maximum.append(duplicated[i])
+        inqueue = set()
+        queue = deque()
+        for j in range(len(n)):
+            if duplicated[j] >= 2 and j != i:
+                queue.append(j)
+                inqueue.add(j)
+
+        while queue:
+            j = queue.popleft()
+            inqueue.remove(j)
+            half = duplicated[j]//2
+            if j - 1 >= 0:
+                duplicated[j-1] += half
+                if duplicated[j - 1] >= 2 and j-1 != i and (j - 1) not in inqueue:
+                    queue.append(j - 1)
+                    inqueue.add(j - 1)
+            if j + 1 < len(n):
+                duplicated[j + 1] += half
+                if duplicated[j + 1] >= 2 and j+1 != i and (j + 1) not in inqueue:
+                    queue.append(j + 1)
+                    inqueue.add(j + 1)
+
+            duplicated[j] %= 2
+
+        maximum[i] = duplicated[i]
+
     return maximum
-    
 
 
 if __name__ == '__main__':
     cases = int(input())
     cases_table = []
-    for i in range (cases):
-        machinescount= int(input())
+    for _ in range(cases):
+        machinescount = int(input())
         st = input()
-        
-        m=[st.split(" ",machinescount)]
-        n=[int(x) for x in m[0]]
+        n = list(map(int, st.split()))
         cases_table.append(n)
-    for i in range(cases):
-        n=CoffeeBreak(cases_table[i])
-        for i in range(len(n)):
-           print(n[i], end=" ")
-        print()
+
+    for case in cases_table:
+        result = CoffeeBreak(case)
+        print(" ".join(map(str, result)))
